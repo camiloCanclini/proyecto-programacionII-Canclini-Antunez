d169f432568db2caa5312638c201b5884cec62c4
agustin-antunez <antunezagustin01@gmail.com>
2022-12-16 17:04:53 -0300

    Se hacen mas  cambios

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
+
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def principal():
@@ -15,9 +16,19 @@ def login():
 @app.route("/user")
 def user():
     return render_template("movie_info.html")
+
+@app.route("/json")
+def get_data():
+    handler = open('Json/cuentas.json', 'r', encoding='utf-8')
+    data = handler.read()
+    file = json.loads(data)
+    return jsonify(file)
+
+
 @app.route("/user/upload_movie")
 def user2():
     return render_template("user_menu_upload_movie.html")
 
+if __name__ == "__main__":
 
-
+    app.run(debug=True)
