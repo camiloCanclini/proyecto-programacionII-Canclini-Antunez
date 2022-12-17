44ced2f65f5eaa2c3566e3dbfda8c56c6c983f32
Camilo Canclini <camilocanclini@gmail.com>
2022-12-16 22:14:59 -0300

    Acomodando Codigo |  Integrando funcion para logearse

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
-from flask import Flask, render_template, jsonify, Response
+from flask import Flask, render_template, jsonify, Response, request
 from http import HTTPStatus
 import json
-
-
-
+"""
++---------------+
++-- FUNCTIONS --+
++---------------+ 
+"""
+def searchUserInJson (username, password, path_json):
+    with open(path_json) as content:
+        usuarios = json.load(content)
+        #print(usuarios)
+        #print(usuarios["users"][0])
+        for i in usuarios['users']:
+            if username == i['username'] and password == i['password']:
+                print("\nUser: ", username, "Logged successfully\n")
+                return True   
+        print("\nUser: ", username, "Was not found\n")
+        return False
+"""
++------------------+
++-- EVENTS FLASK --+
++------------------+ 
+"""
 app = Flask(__name__)
 
+"""
++-- INDEX PAGE --+
+"""
 @app.route("/")
 def principal():
-    return render_template("principal.html")
-
-
+    return render_template("index.html")
+"""
++-- LOGIN USERS --+
+"""
 @app.route("/login")
 def login():
-    return render_template("login_user.html")
-
-@app.route("/user")
-def user():
-    return render_template("movie_info.html")
-
-@app.route("/prueba", methods=["GET"])
-def get_data():
-    with open('Json/cuentas.json') as content:
-        usuarios = json.load(content)
-    for usuario in usuarios['users']:
-        if usuario['username'] == "admin":
-            return usuario['username']
-        else:
-            return Response("{}",HTTPStatus.NOT_EXTENDED)
-        
-
+    return render_template("login.html")
+@app.route("/user", methods=['POST'])
+def user(): 
+    parametros = request.form
+    #print("\nLOS PARAMETROS SON: ", parametros,"\n")
+    username = parametros['username']
+    password = parametros['pass']
+    if searchUserInJson(username, password, 'database/cuentas.json'):
+        return render_template('movie_info.html')
+    else:
+        return render_template('login_user.html')
+"""
++-- LOGGED USERS --+
+"""
 @app.route("/user/upload_movie")
 def user2():
     return render_template("user_menu_upload_movie.html")
-
+"""
++---------------------+
++-- BOOT APP SERVER --+
++---------------------+ 
+"""
 if __name__ == "__main__":
-
     app.run(debug=True)
